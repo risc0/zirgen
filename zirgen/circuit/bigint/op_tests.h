004f94239958b3310fa6e0551ee74d4cbb17fbbb
Mars Saxman <112983328+mars-risc0@users.noreply.github.com>
2024-08-26 12:13:55 -0700

    delete unused test support function which prompts a compiler warning (#21)

--- zirgen/circuit/bigint/op_tests.h
+++ zirgen/circuit/bigint/op_tests.h
@@ -24,7 +24,6 @@ void makeConstAddTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeConstAddAltTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeConstMulTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeAddTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
-void makeConstZeroTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeConstOneTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeConstTwoByteTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
 void makeSubTest(mlir::OpBuilder builder, mlir::Location loc, size_t bits);
