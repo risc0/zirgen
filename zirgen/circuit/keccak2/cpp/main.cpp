ecbfb28469b8fc8e158369a478801783a8b69167
Jeremy Bruestle <jeremy@risczero.com>
2024-11-22 14:46:04 -0800

    Almost done

--- zirgen/circuit/keccak2/cpp/main.cpp
+++ zirgen/circuit/keccak2/cpp/main.cpp
@@ -3,8 +3,13 @@
 // All rights reserved.
 
 #include "zirgen/circuit/keccak2/cpp/run.h"
+#include "zirgen/compiler/zkp/sha256.h"
 
 int main() {
+  zirgen::Digest digest = zirgen::impl::initState();
+  std::vector<uint32_t> data(16, 0);
+  data[0] = 0;
+  zirgen::impl::compress(digest, data.data());
   using namespace zirgen::keccak2;
   std::vector<KeccakState> inputs;
   KeccakState test;
