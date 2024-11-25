a897093ba500752e46fdbc3bd851e669429f8a5b
shkoo <nils@risczero.com>
2024-11-25 13:50:05 -0800

    Add `--check` and `--dry-run` options to bootstrap (#91)
    --check is suitable for use in CI to test that files are up to date

--- zirgen/bootstrap/src/main.rs
+++ zirgen/bootstrap/src/main.rs
@@ -12,16 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use anyhow::{bail, Result};
+use clap::{Parser, ValueEnum};
+use glob::Pattern;
+use regex::Regex;
 use std::{
+    cell::RefCell,
     collections::BTreeMap,
     io,
     io::{BufRead, BufReader},
     path::{Path, PathBuf},
     process::{exit, Command, Stdio},
 };
-
-use clap::{Parser, ValueEnum};
-use glob::Pattern;
 
 #[derive(Debug)]
 struct Rule<'a> {
@@ -53,41 +55,6 @@ impl<'a> Rule<'a> {
             base_suffix,
             ..self
         }
-    }
-
-    fn apply_to(&self, src_file: impl AsRef<Path>, dest_base: impl AsRef<Path>) -> bool {
-        let src_file = src_file.as_ref().to_path_buf();
-        let mut dest_base = dest_base.as_ref().to_path_buf();
-
-        let src_fn = src_file
-            .file_name()
-            .expect("Empty filename?")
-            .to_str()
-            .unwrap();
-
-        if !Pattern::new(self.pattern).unwrap().matches(src_fn) {
-            return false;
-        }
-        if !self.base_suffix.is_empty() {
-            dest_base.set_file_name(
-                [
-                    dest_base.file_name().unwrap().to_str().unwrap(),
-                    self.base_suffix,
-                ]
-                .join(""),
-            )
-        }
-        let dest_fn;
-        if src_fn.starts_with(self.remove_prefix) {
-            dest_fn = &src_fn[self.remove_prefix.len()..]
-        } else {
-            dest_fn = &src_fn
-        }
-        copy(
-            &src_file,
-            &dest_base.join(self.dest_dir).join(Path::new(dest_fn)),
-        );
-        true
     }
 }
 
@@ -154,6 +121,22 @@ struct Args {
     /// When bootstapping the risc0 monorepo, this should be the path to the repo root.
     #[clap(long)]
     output: Option<PathBuf>,
+
+    /// Show what would have been done instead of actually doing it.
+    #[clap(long)]
+    dry_run: bool,
+
+    /// Instead of installing, check to make sure installed files are
+    /// up to date.  Exits with an error if they're not.
+    #[clap(long)]
+    check: bool,
+}
+
+struct Bootstrap {
+    args: Args,
+
+    /// True if we've encountered an error.
+    error: RefCell<bool>,
 }
 
 fn get_bazel_bin() -> PathBuf {
@@ -168,67 +151,16 @@ fn get_bazel_bin() -> PathBuf {
     Path::new(bazel_bin.trim()).to_path_buf()
 }
 
-fn copy_group(
-    circuit: &str,
-    src_path: &Path,
-    out_dir: &Option<PathBuf>,
-    outputs: &[&str],
-    child_dir: &str,
-    prefix: &str,
-) {
-    let bazel_bin = get_bazel_bin();
-    for filename in outputs {
-        let src_filename = format!("{prefix}{filename}");
-        let src_path = bazel_bin.join(src_path).join(src_filename);
-        let tgt_dir = if let Some(ref out_dir) = out_dir {
-            PathBuf::from(out_dir)
-        } else {
-            PathBuf::from("zirgen/circuit").join(circuit)
-        }
-        .join(child_dir);
-
-        copy(&src_path, &tgt_dir.join(filename));
-    }
-}
-
-fn copy_file(src_dir: &Path, tgt_dir: &Path, filename: &str) {
-    copy(&src_dir.join(filename), &tgt_dir.join(filename))
-}
-
-fn copy(src_path: &Path, tgt_path: &Path) {
-    println!("{} -> {}", src_path.display(), tgt_path.display());
-
-    let tgt_dir = tgt_path.parent().unwrap();
-    std::fs::create_dir_all(&tgt_dir).unwrap();
-
-    // Avoid using `std::fs::copy` because bazel creates files with r/o permissions.
-    // We create a new file with r/w permissions and copy the contents instead.
-    let mut src = std::fs::File::open(src_path)
-        .expect(&format!("Could not open source: {}", src_path.display()));
-    let mut tgt = std::fs::File::create(&tgt_path).unwrap();
-    io::copy(&mut src, &mut tgt).unwrap();
-
-    let filename = tgt_path.file_name().unwrap().to_str().unwrap();
-    if filename.ends_with(".rs.inc") {
-        // Cargo will not format these automatically, so do so by hand
-        Command::new("rustfmt")
-            .args(tgt_path.to_str())
-            .status()
-            .expect(&format!("Unable to format {}", tgt_path.display()));
-    }
-
-    if filename.ends_with(".cpp.inc") || filename.ends_with(".cu.inc") {
-        // Cargo will not format these automatically, so do so by hand
-        if let Err(err) = Command::new("clang-format")
-            .args(["-i", tgt_path.to_str().unwrap()])
-            .status()
-        {
-            eprintln!(
-                "Warning: unable to format {} with clang-format: {err}",
-                tgt_path.display()
-            );
-        }
-    }
+fn strip_locations_and_whitespace(text: &str) -> Result<String> {
+    // replace everything that might be a line number with '*'
+    let re = Regex::new(":[0-9]+")?;
+    let stripped = re.replace_all(text, "*");
+    // Remove all whitespace to avoid noticing reformatting changes
+    Ok(stripped
+        .as_ref()
+        .chars()
+        .filter(|&c| c != ' ' && c == '\t' && c != '\n')
+        .collect())
 }
 
 fn cargo_fmt_circuit(circuit: &str, tgt_dir: &Option<PathBuf>, manifest: &Option<PathBuf>) {
@@ -259,9 +191,173 @@ fn cargo_fmt(manifest: &Path) {
     }
 }
 
-impl Args {
+impl Bootstrap {
+    fn new(args: Args) -> Self {
+        Self {
+            args,
+            error: RefCell::new(false),
+        }
+    }
+
+    fn apply_rule(
+        &self,
+        rule: &Rule,
+        src_file: impl AsRef<Path>,
+        dest_base: impl AsRef<Path>,
+    ) -> bool {
+        let src_file = src_file.as_ref().to_path_buf();
+        let mut dest_base = dest_base.as_ref().to_path_buf();
+
+        let src_fn = src_file
+            .file_name()
+            .expect("Empty filename?")
+            .to_str()
+            .unwrap();
+
+        if !Pattern::new(rule.pattern).unwrap().matches(src_fn) {
+            return false;
+        }
+        if !rule.base_suffix.is_empty() {
+            dest_base.set_file_name(
+                [
+                    dest_base.file_name().unwrap().to_str().unwrap(),
+                    rule.base_suffix,
+                ]
+                .join(""),
+            )
+        }
+        let dest_fn;
+        if src_fn.starts_with(rule.remove_prefix) {
+            dest_fn = &src_fn[rule.remove_prefix.len()..]
+        } else {
+            dest_fn = &src_fn
+        }
+        self.copy(
+            &src_file,
+            &dest_base.join(rule.dest_dir).join(Path::new(dest_fn)),
+        );
+        true
+    }
+    fn copy_group(
+        &self,
+        circuit: &str,
+        src_path: &Path,
+        out_dir: &Option<PathBuf>,
+        outputs: &[&str],
+        child_dir: &str,
+        prefix: &str,
+    ) {
+        let bazel_bin = get_bazel_bin();
+        for filename in outputs {
+            let src_filename = format!("{prefix}{filename}");
+            let src_path = bazel_bin.join(src_path).join(src_filename);
+            let tgt_dir = if let Some(ref out_dir) = out_dir {
+                PathBuf::from(out_dir)
+            } else {
+                PathBuf::from("zirgen/circuit").join(circuit)
+            }
+            .join(child_dir);
+
+            self.copy(&src_path, &tgt_dir.join(filename));
+        }
+    }
+
+    fn copy_file(&self, src_dir: &Path, tgt_dir: &Path, filename: &str) {
+        self.copy(&src_dir.join(filename), &tgt_dir.join(filename))
+    }
+
+    fn check(&self, src_path: &Path, tgt_path: &Path) -> Result<()> {
+        let src = std::fs::read(src_path)?;
+        let dst = std::fs::read(tgt_path)?;
+
+        let src_fn = src_path.file_name().unwrap().to_str().unwrap();
+
+        let textual: bool = src_fn.ends_with(".cu")
+            || src_fn.ends_with(".h")
+            || src_fn.ends_with(".inc")
+            || src_fn.ends_with(".cuh")
+            || src_fn.ends_with(".cpp")
+            || src_fn.ends_with(".rs");
+
+        if textual {
+            // Things might change like line numbers or whitespace that don't have a functional impact.
+            let src = strip_locations_and_whitespace(&String::from_utf8(src)?)?;
+            let dst = strip_locations_and_whitespace(&String::from_utf8(dst)?)?;
+            if src != dst {
+                bail!(
+                    "Mismatch: {} != {} (text)",
+                    src_path.display(),
+                    tgt_path.display()
+                )
+            }
+        } else {
+            if src != dst {
+                bail!(
+                    "Mismatch: {} != {} (binary)",
+                    src_path.display(),
+                    tgt_path.display()
+                )
+            }
+        }
+
+        Ok(())
+    }
+
+    fn copy(&self, src_path: &Path, tgt_path: &Path) {
+        if self.args.dry_run {
+            println!(
+                "{} -> {} (dry run, skipping)",
+                src_path.display(),
+                tgt_path.display()
+            );
+            return;
+        } else if self.args.check {
+            if let Err(err) = self.check(src_path, tgt_path) {
+                eprintln!("Error: {}", err);
+                *self.error.borrow_mut() = true;
+            };
+        } else {
+            println!("{} -> {}", src_path.display(), tgt_path.display());
+            let tgt_dir = tgt_path.parent().unwrap();
+            std::fs::create_dir_all(&tgt_dir).unwrap();
+
+            // Avoid using `std::fs::copy` because bazel creates files with r/o permissions.
+            // We create a new file with r/w permissions and .copy the contents instead.
+            let mut src = std::fs::File::open(src_path)
+                .expect(&format!("Could not open source: {}", src_path.display()));
+            let mut tgt = std::fs::File::create(&tgt_path).unwrap();
+            io::copy(&mut src, &mut tgt).unwrap();
+
+            let filename = tgt_path.file_name().unwrap().to_str().unwrap();
+            if filename.ends_with(".rs.inc") || filename.ends_with(".rs") {
+                Command::new("rustfmt")
+                    .args(tgt_path.to_str())
+                    .status()
+                    .expect(&format!("Unable to format {}", tgt_path.display()));
+            }
+
+            if filename.ends_with(".cpp.inc")
+                || filename.ends_with(".cu.inc")
+                || filename.ends_with(".cuh")
+            {
+                if let Err(err) = Command::new("clang-format")
+                    .args(["-i", tgt_path.to_str().unwrap()])
+                    .status()
+                {
+                    eprintln!(
+                        "Warning: unable to format {} with clang-format: {err}",
+                        tgt_path.display()
+                    );
+                }
+            }
+        }
+    }
+
     fn output_or(&self, alternative: &str) -> PathBuf {
-        self.output.clone().unwrap_or(PathBuf::from(alternative))
+        self.args
+            .output
+            .clone()
+            .unwrap_or(PathBuf::from(alternative))
     }
 
     fn build_all_circuits(&self) {
@@ -317,7 +413,7 @@ impl Args {
 
             let mut found_rule = false;
             for (idx, rule) in install_rules.iter().enumerate() {
-                if rule.apply_to(&path, dest_base) {
+                if self.apply_rule(rule, &path, dest_base) {
                     found_rule = true;
                     rules_used[idx] = true;
                 }
@@ -342,7 +438,7 @@ impl Args {
     }
 
     fn run(&self) {
-        match self.circuit {
+        match self.args.circuit {
             Circuit::Fib => self.fib(),
             Circuit::Predicates => self.predicates(),
             Circuit::Recursion => self.recursion(),
@@ -352,6 +448,20 @@ impl Args {
             Circuit::Verify => self.stark_verify(),
             Circuit::BigInt => self.bigint(),
             Circuit::BigInt2 => self.bigint2(),
+        }
+
+        if self.args.check {
+            if *self.error.borrow() {
+                eprintln!("--check: Mismatches encountered");
+                // Ado a different error message if we have errors during other operations
+                exit(1);
+            } else {
+                eprintln!("--check: All installed files match");
+            }
+        } else {
+            if *self.error.borrow() {
+                panic!("Please add an appropriate error message here if we have defererred errors during a non-check operation");
+            }
         }
     }
 
@@ -366,19 +476,19 @@ impl Args {
                 Rule::copy("*.metal", "kernels"),
             ],
         );
-        cargo_fmt_circuit("fib", &self.output, &None);
+        cargo_fmt_circuit("fib", &self.args.output, &None);
     }
 
     fn predicates(&self) {
         self.build_all_circuits();
 
         let bazel_bin = get_bazel_bin();
-        let risc0_root = self.output.as_ref().expect("--output is required");
+        let risc0_root = self.args.output.as_ref().expect("--output is required");
         let risc0_root = risc0_root.join("risc0");
         let rust_path = risc0_root.join("circuit/recursion");
         let zkr_src_path = bazel_bin.join("zirgen/circuit/predicates");
         let zkr_tgt_path = rust_path.join("src");
-        copy_file(&zkr_src_path, &zkr_tgt_path, RECURSION_ZKR_ZIP);
+        self.copy_file(&zkr_src_path, &zkr_tgt_path, RECURSION_ZKR_ZIP);
     }
 
     fn recursion(&self) {
@@ -416,11 +526,11 @@ impl Args {
             self.output_or("zirgen/dsl/examples/calculator"),
             &[Rule::copy("*.inc", ""), Rule::copy("*.rs", "")],
         );
-        cargo_fmt_circuit("calculator", &self.output, &None);
+        cargo_fmt_circuit("calculator", &self.args.output, &None);
     }
 
     fn copy_edsl_style(&self, circuit: &str, src_dir: &str) {
-        let risc0_root = self.output.as_ref().expect("--output is required");
+        let risc0_root = self.args.output.as_ref().expect("--output is required");
         let risc0_root = risc0_root.join("risc0");
         let src_path = Path::new(src_dir);
         let rust_path = risc0_root.join("circuit").join(circuit);
@@ -431,7 +541,7 @@ impl Args {
         let hal_root = Some(sys_path.join("kernels"));
         let sys_path = Some(sys_path);
 
-        copy_group(
+        self.copy_group(
             circuit,
             &src_path,
             &sys_path,
@@ -439,11 +549,11 @@ impl Args {
             "cxx",
             "rust_",
         );
-        copy_group(circuit, &src_path, &rust_path, MAIN_RUST_OUTPUTS, "src", "");
-        copy_group(circuit, &src_path, &hal_root, CUDA_OUTPUTS, "cuda", "");
-        copy_group(circuit, &src_path, &hal_root, METAL_OUTPUTS, "metal", "");
+        self.copy_group(circuit, &src_path, &rust_path, MAIN_RUST_OUTPUTS, "src", "");
+        self.copy_group(circuit, &src_path, &hal_root, CUDA_OUTPUTS, "cuda", "");
+        self.copy_group(circuit, &src_path, &hal_root, METAL_OUTPUTS, "metal", "");
 
-        copy_group(
+        self.copy_group(
             circuit,
             &src_path,
             &sys_path,
@@ -451,7 +561,7 @@ impl Args {
             "cxx",
             "",
         );
-        copy_group(
+        self.copy_group(
             circuit,
             &src_path,
             &hal_root,
@@ -466,15 +576,15 @@ impl Args {
         self.build_all_circuits();
 
         let bazel_bin = get_bazel_bin();
-        let risc0_root = self.output.as_ref().expect("--output is required");
+        let risc0_root = self.args.output.as_ref().expect("--output is required");
         let inc_path = Path::new("zirgen/circuit/verify/circom/include");
         let src_path = bazel_bin.join("zirgen/circuit/verify/circom");
         let tgt_path = risc0_root.join("groth16_proof/groth16");
         let rust_path = risc0_root.join("risc0/groth16/src");
 
-        copy_file(&inc_path, &tgt_path, "risc0.circom");
-        copy_file(&src_path, &tgt_path, "stark_verify.circom");
-        copy_file(&src_path, &rust_path, "seal_format.rs");
+        self.copy_file(&inc_path, &tgt_path, "risc0.circom");
+        self.copy_file(&src_path, &tgt_path, "stark_verify.circom");
+        self.copy_file(&src_path, &rust_path, "seal_format.rs");
         cargo_fmt(&risc0_root.join("risc0/groth16/Cargo.toml"));
     }
 
@@ -482,7 +592,7 @@ impl Args {
         self.build_all_circuits();
 
         let circuit = "bigint";
-        let risc0_root = self.output.as_ref().expect("--output is required");
+        let risc0_root = self.args.output.as_ref().expect("--output is required");
         let risc0_root = risc0_root.join("risc0");
         let bigint_crate_root = risc0_root.join("circuit/bigint");
         let out = bigint_crate_root.join("src");
@@ -490,8 +600,8 @@ impl Args {
         let bazel_bin = get_bazel_bin();
         let src_path = bazel_bin.join("zirgen/circuit/bigint");
 
-        copy_file(&src_path, &out, BIGINT_ZKR_ZIP);
-        copy_group(
+        self.copy_file(&src_path, &out, BIGINT_ZKR_ZIP);
+        self.copy_group(
             circuit,
             &src_path,
             &Some(out.clone()),
@@ -538,19 +648,19 @@ impl Args {
     fn bigint2(&self) {
         self.build_all_circuits();
 
-        let risc0_root = self.output.as_ref().expect("--output is required");
+        let risc0_root = self.args.output.as_ref().expect("--output is required");
         let risc0_root = risc0_root.join("risc0");
         let bazel_bin = get_bazel_bin();
         let src_path = bazel_bin.join("zirgen/circuit/bigint");
         let rsa_path = risc0_root.join("bigint2/src/rsa");
         let ec_path = risc0_root.join("bigint2/src/ec");
 
-        copy_file(&src_path, &rsa_path, "modpow_65537.blob");
-        copy(
+        self.copy_file(&src_path, &rsa_path, "modpow_65537.blob");
+        self.copy(
             &src_path.join("ec_double.blob"),
             &ec_path.join("double.blob"),
         );
-        copy(&src_path.join("ec_add.blob"), &ec_path.join("add.blob"));
+        self.copy(&src_path.join("ec_add.blob"), &ec_path.join("add.blob"));
     }
 }
 
@@ -567,7 +677,6 @@ fn bazel_config() -> &'static str {
 
 fn main() {
     env_logger::init();
-    let args = Args::parse();
-
-    args.run();
+    let bootstrap = Bootstrap::new(Args::parse());
+    bootstrap.run();
 }
