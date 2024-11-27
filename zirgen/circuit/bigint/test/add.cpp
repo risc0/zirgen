1f0d19542909282d8f6828759d483a727a3657dc
Mars Saxman <112983328+mars-risc0@users.noreply.github.com>
2024-11-27 13:19:30 -0800

    ZIR-255: Restore bigint op tests via bibc (#96)
    * move bigint op tests over to more appropriate home directory
    
    * extract bibc test apparatus into its own file
    
    * move test support code out of test file
    
    * set up lib so groups of tests can have their own files; split off rsa test
    
    * separate add and mul tests into their own modules
    
    * const_add op test
    
    * add const_add_alt test
    
    * add const_mul bigint op test
    
    * add const_one op test
    
    * add const_twobyte op test
    
    * remove duplicate add tests mistakenly copied into mul.cpp
    
    * add sub test from op tests
    
    * add reduce test from op tests
    
    * add nondet_inv test from op_tests
    
    * clang-format

---
+++ zirgen/circuit/bigint/test/add.cpp
@@ -0,0 +1,55 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "zirgen/circuit/bigint/op_tests.h"
+#include "zirgen/circuit/bigint/rsa.h"
+#include "zirgen/circuit/bigint/test/bibc.h"
+
+#include <gtest/gtest.h>
+
+using namespace zirgen;
+using namespace zirgen::BigInt::test;
+
+TEST_F(BibcTest, Add8) {
+  mlir::OpBuilder builder(ctx);
+  auto func = makeFunc("add_8", builder);
+  BigInt::makeAddTest(builder, func.getLoc(), 8);
+
+  auto inputs = apints({"1", "2", "3"});
+  ZType a, b;
+  AB(func, inputs, a, b);
+  EXPECT_EQ(a, b);
+}
+
+TEST_F(BibcTest, Add16) {
+  mlir::OpBuilder builder(ctx);
+  auto func = makeFunc("add_16", builder);
+  BigInt::makeAddTest(builder, func.getLoc(), 16);
+
+  auto inputs = apints({"1", "2", "3"});
+  ZType a, b;
+  AB(func, inputs, a, b);
+  EXPECT_EQ(a, b);
+}
+
+TEST_F(BibcTest, Add128) {
+  mlir::OpBuilder builder(ctx);
+  auto func = makeFunc("add_128", builder);
+  BigInt::makeAddTest(builder, func.getLoc(), 128);
+
+  auto inputs = apints({"1", "2", "3"});
+  ZType a, b;
+  AB(func, inputs, a, b);
+  EXPECT_EQ(a, b);
+}
---
+++ zirgen/circuit/bigint/test/rsa.cpp
@@ -0,0 +1,56 @@
+// Copyright 2024 RISC Zero, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "zirgen/circuit/bigint/rsa.h"
+#include "zirgen/circuit/bigint/op_tests.h"
+#include "zirgen/circuit/bigint/test/bibc.h"
+
+#include <gtest/gtest.h>
+
+using namespace zirgen;
+using namespace zirgen::BigInt::test;
+
+TEST_F(BibcTest, RSA256) {
+  mlir::OpBuilder builder(ctx);
+  auto func = makeFunc("rsa_256", builder);
+  BigInt::makeRSA(builder, func.getLoc(), 256);
+  lower();
+
+  llvm::APInt N(64, 101);
+  llvm::APInt S(64, 32766);
+  llvm::APInt M(64, 53);
+  EXPECT_EQ(M, BigInt::RSA(N, S));
+  std::vector<llvm::APInt> inputs = {N, S, M};
+
+  ZType a, b;
+  AB(func, inputs, a, b);
+  EXPECT_EQ(a, b);
+}
+
+TEST_F(BibcTest, RSA3072) {
+  mlir::OpBuilder builder(ctx);
+  auto func = makeFunc("rsa_3072", builder);
+  BigInt::makeRSA(builder, func.getLoc(), 3072);
+  lower();
+
+  llvm::APInt N(64, 22764235167642101);
+  llvm::APInt S(64, 10116847215);
+  llvm::APInt M(64, 14255570451702775);
+  EXPECT_EQ(M, BigInt::RSA(N, S));
+  std::vector<llvm::APInt> inputs = {N, S, M};
+
+  ZType a, b;
+  AB(func, inputs, a, b);
+  EXPECT_EQ(a, b);
+}
