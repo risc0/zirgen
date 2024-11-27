9f7ca2fdec1547db1f64c7dd67754217094bda63
Mars Saxman <mars@risczero.com>
2024-11-27 10:20:47 -0800

    set up lib so groups of tests can have their own files; split off rsa test

--- zirgen/circuit/bigint/test/test.cpp
+++ zirgen/circuit/bigint/test/test.cpp
@@ -87,36 +87,3 @@ TEST_F(BibcTest, Mul128) {
   EXPECT_EQ(a, b);
 }
 
-TEST_F(BibcTest, RSA256) {
-  mlir::OpBuilder builder(ctx);
-  auto func = makeFunc("rsa_256", builder);
-  BigInt::makeRSA(builder, func.getLoc(), 256);
-  lower();
-
-  llvm::APInt N(64, 101);
-  llvm::APInt S(64, 32766);
-  llvm::APInt M(64, 53);
-  EXPECT_EQ(M, BigInt::RSA(N, S));
-  std::vector<llvm::APInt> inputs = {N, S, M};
-
-  ZType a, b;
-  AB(func, inputs, a, b);
-  EXPECT_EQ(a, b);
-}
-
-TEST_F(BibcTest, RSA3072) {
-  mlir::OpBuilder builder(ctx);
-  auto func = makeFunc("rsa_3072", builder);
-  BigInt::makeRSA(builder, func.getLoc(), 3072);
-  lower();
-
-  llvm::APInt N(64, 22764235167642101);
-  llvm::APInt S(64, 10116847215);
-  llvm::APInt M(64, 14255570451702775);
-  EXPECT_EQ(M, BigInt::RSA(N, S));
-  std::vector<llvm::APInt> inputs = {N, S, M};
-
-  ZType a, b;
-  AB(func, inputs, a, b);
-  EXPECT_EQ(a, b);
-}
