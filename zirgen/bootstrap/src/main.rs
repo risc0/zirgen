3f49e6d48dc8efd80a21b55a756aaac3f31c44f4
Paul Gafni <paul.gafni@gmail.com>
2024-12-14 13:37:54 -0800

    adding risc0 output to bootstrapping

--- zirgen/bootstrap/src/main.rs
+++ zirgen/bootstrap/src/main.rs
@@ -608,10 +608,12 @@ impl Bootstrap {
         let src_path = bazel_bin.join("zirgen/circuit/bigint");
         let rsa_path = risc0_root.join("bigint2/src/rsa");
         let ec_path = risc0_root.join("bigint2/src/ec");
+        let add128_path = risc0_root.join("bigint2/src/add128");
 
         self.copy_file(&src_path, &rsa_path, "modpow65537_4096.blob");
         self.copy_file(&src_path, &ec_path, "ec_add_256.blob");
         self.copy_file(&src_path, &ec_path, "ec_double_256.blob");
+        self.copy_file(&src_path, &add128_path, "add128.blob");
     }
 }
 
