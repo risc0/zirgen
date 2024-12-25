866f0985be7fe8cddf8aaaf81ac32721b57b5190
Jeremy Bruestle <jeremy@risczero.com>
2024-12-25 07:18:55 -0800

    Improve sha tests to cover all the relevant cases

--- zirgen/circuit/rv32im/v2/test/test_sha_kernel.cpp
+++ zirgen/circuit/rv32im/v2/test/test_sha_kernel.cpp
@@ -46,6 +46,20 @@ static constexpr uint32_t SHA_K[64] = {
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 };
 
+inline void do_sha2(uint32_t stateIn, uint32_t stateOut, uint32_t data, uint32_t count) {
+  register uintptr_t a0 asm("a0") = stateIn;
+  register uintptr_t a1 asm("a1") = stateOut;
+  register uintptr_t a2 asm("a2") = data;
+  register uintptr_t a3 asm("a3") = count;
+  register uintptr_t a4 asm("a4") = (uint32_t)SHA_K;
+  register uintptr_t a7 asm("a7") = 4;
+  asm volatile("ecall\n"
+               :                                                      // no outputs
+               : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a7) // inputs
+               :                                                      // no clobbers
+  );
+}
+
 constexpr uint32_t swap(uint32_t n) {
   return (((n & 0x000000ff) << 24) | ((n & 0x0000ff00) << 8) | ((n & 0x00ff0000) >> 8) |
           ((n & 0xff000000) >> 24));
@@ -62,45 +76,81 @@ static constexpr uint32_t SHA_INIT[8] = {
     swap(0x5be0cd19),
 };
 
-inline void do_sha2(uint32_t stateIn, uint32_t stateOut, uint32_t data, uint32_t count) {
-  register uintptr_t a0 asm("a0") = stateIn;
-  register uintptr_t a1 asm("a1") = stateOut;
-  register uintptr_t a2 asm("a2") = data;
-  register uintptr_t a3 asm("a3") = count;
-  register uintptr_t a4 asm("a4") = (uint32_t)SHA_K;
-  register uintptr_t a7 asm("a7") = 4;
-  asm volatile("ecall\n"
-               :                                                      // no outputs
-               : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a7) // inputs
-               :                                                      // no clobbers
-  );
+static constexpr uint8_t parseHex(char x) {
+  if (x >= 'a' && x <= 'f') {
+    return 10 + x - 'a';
+  }
+  if (x >= '0' && x <= '9') {
+    return x - '0';
+  }
+  die();
+  return 0;
 }
 
-void test_sha_basic() {
-  uint8_t state[32];
-  uint8_t data[64];
+void compareHex(uint32_t* words, const char* str) {
+  uint8_t* asBytes = reinterpret_cast<uint8_t*>(words);
   for (size_t i = 0; i < 32; i++) {
-    data[i] = 0;
-  }
-  data[0] = 0x61; // a
-  data[1] = 0x62; // b
-  data[2] = 0x63; // c
-  data[3] = 0x80; // One bit
-  data[63] = 24;  // 24 bits
-  constexpr uint8_t goal[32] = {
-      0xba, 0x78, 0x16, 0xbf, 0x8f, 0x01, 0xcf, 0xea, 0x41, 0x41, 0x40,
-      0xde, 0x5d, 0xae, 0x22, 0x23, 0xb0, 0x03, 0x61, 0xa3, 0x96, 0x17,
-      0x7a, 0x9c, 0xb4, 0x10, 0xff, 0x61, 0xf2, 0x00, 0x15, 0xad,
-  };
-  do_sha2((uint32_t)SHA_INIT, (uint32_t)state, (uint32_t)data, 1);
-  for (size_t i = 0; i < 32; i++) {
-    if (state[i] != goal[i]) {
+    uint8_t highNibble = parseHex(*str++);
+    uint8_t lowNibble = parseHex(*str++);
+    if (asBytes[i] != highNibble * 16 + lowNibble) {
       die();
     }
   }
 }
 
+void shaPad(uint8_t* out, const char* in) {
+  uint32_t bits = 0;
+  while (*in) {
+    *out++ = *in++;
+    bits += 8;
+  }
+  uint32_t outBits = bits;
+  *out++ = 0x80;
+  bits += 8;
+  while (bits % 512 != 0) {
+    *out++ = 0;
+    bits += 8;
+  }
+  out -= 2;
+  out[0] = outBits / 256;
+  out[1] = outBits % 256;
+}
+
+void test_sha_zero_blocks() {
+  uint32_t state[8];
+
+  do_sha2((uint32_t)SHA_INIT, (uint32_t)state, 0, 0);
+
+  compareHex(state, "6a09e667bb67ae853c6ef372a54ff53a510e527f9b05688c1f83d9ab5be0cd19");
+}
+
+void test_sha_one_block() {
+  uint32_t state[8];
+  uint64_t data[16];
+
+  shaPad(reinterpret_cast<uint8_t*>(data), "abc");
+
+  do_sha2((uint32_t)SHA_INIT, (uint32_t)state, (uint32_t)data, 1);
+
+  compareHex(state, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad");
+}
+
+void test_sha_two_blocks() {
+  uint32_t state[8];
+  uint64_t data[32];
+
+  shaPad(reinterpret_cast<uint8_t*>(data),
+         "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrl"
+         "mnopqrsmnopqrstnopqrstu");
+
+  do_sha2((uint32_t)SHA_INIT, (uint32_t)state, (uint32_t)data, 2);
+
+  compareHex(state, "cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1");
+}
+
 extern "C" void start() {
-  test_sha_basic();
+  test_sha_zero_blocks();
+  test_sha_one_block();
+  test_sha_two_blocks();
   terminate(0);
 }
