deec7bbdd47958c4cfddae23445e71aec0ca03bf
jacobdweightman <jacob@risczero.com>
2025-02-20 11:04:43 -0600

    Optimize emitted Picus signals

--- zirgen/dsl/passes/InlineForPicus.cpp
+++ zirgen/dsl/passes/InlineForPicus.cpp
@@ -83,8 +83,8 @@ struct InlineForPicusPass : public InlineForPicusBase<InlineForPicusPass> {
       auto op = cast<Zhlt::ConstructOp>(call.call);
       auto callee = op.getCallee();
       return callee == "Add" || callee == "BitAnd" || callee == "Component" ||
-             callee == "InRange" || callee == "Inv" || callee == "Mod" || callee == "Mul" ||
-             callee == "NondetReg" || callee == "Sub" || callee == "Val";
+             callee == "InRange" || callee == "Inv" || callee == "Isz" || callee == "Mod" ||
+             callee == "Mul" || callee == "NondetReg" || callee == "Sub" || callee == "Val";
     };
 
     // Get an instance of the inliner.
